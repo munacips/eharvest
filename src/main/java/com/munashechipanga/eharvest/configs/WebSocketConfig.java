56489eccb6ae13b5d57f86990a1efdb6124de03f
munacips <munacips@gmail.com>
2026-06-02 02:05:51 +0200

    Fix II on websockets

--- src/main/java/com/munashechipanga/eharvest/configs/WebSocketConfig.java
+++ src/main/java/com/munashechipanga/eharvest/configs/WebSocketConfig.java
@@ -1,7 +1,16 @@
 package com.munashechipanga.eharvest.configs;
 
+import com.munashechipanga.eharvest.security.JwtUtil;
+import io.jsonwebtoken.Claims;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,6 +19,12 @@ import org.springframework.web.socket.config.annotation.WebSocketTransportRegist
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtUtil jwtUtil;
+
+    public WebSocketConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -40,5 +55,31 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setSendBufferSizeLimit(512 * 1024) // 512KB send buffer
                 .setTimeToFirstMessage(30000);    // 30 seconds to first message
     }
-}
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(
+                        message,
+                        StompHeaderAccessor.class
+                );
+
+                if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String authorization = accessor.getFirstNativeHeader("Authorization");
+                    if (authorization != null && authorization.startsWith("Bearer ")) {
+                        String token = authorization.substring(7);
+                        Claims claims = jwtUtil.extractAllClaims(token);
+                        Object userId = claims.get("userId");
+                        if (userId != null) {
+                            accessor.setUser(() -> userId.toString());
+                        }
+                    }
+                }
+
+                return message;
+            }
+        });
+    }
+}
